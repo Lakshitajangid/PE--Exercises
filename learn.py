ca1e38ca0ea534e83f7ef65b51f3c4d9dbf4a1ec
Lakshita jangid <lakshita.jangid.111@gmail.com>
2023-06-13 04:50:22 -0700

    initial commit

---
+++ learn.py
@@ -0,0 +1,10 @@
+def calculate_sales():
+    print("Welcome to the Daily Sales Calculator!")
+    sales_input = input("Please enter the sales amounts for today separated by spaces: ")
+    sales_list = sales_input.split()
+    total_sales = sum(sales_list)
+    average_sales = total_sales / len(sales_list)
+    print("Thank you for providing the sales data.")
+    print("Today's total sales amount is: Rupees", total_sales)
+    print("The average sales per transaction is:"(average_sales))
+calculate_sales()
